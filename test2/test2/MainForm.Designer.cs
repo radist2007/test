31a7e207485b0e966be35b7da24a4cf75ac66349
Alex <izofatov2007@gmail.com>
2019-02-01 16:19:19 +0200

    test

---
+++ test2/test2/MainForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Администратор
+ * Date: 25.11.2016
+ * Time: 0:16
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace test2
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.WebBrowser webBrowser1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+			this.SuspendLayout();
+			// 
+			// webBrowser1
+			// 
+			this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+			this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowser1.Name = "webBrowser1";
+			this.webBrowser1.Size = new System.Drawing.Size(609, 295);
+			this.webBrowser1.TabIndex = 0;
+			this.webBrowser1.Url = new System.Uri("https://www.youtube.com/watch?v=w76hgcNFu8A#t=198.782647", System.UriKind.Absolute);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(609, 295);
+			this.Controls.Add(this.webBrowser1);
+			this.Name = "MainForm";
+			this.Text = "test2";
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ test2/test2/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Администратор
+ * Date: 25.11.2016
+ * Time: 0:16
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace test2
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
